18f8b57f5901c865f459946aecac9cd66df09524
Senrioty <642340273@qq.com>
2019-09-15 16:44:23 +0800

    引用django-mptt第三方库来做树形结构 从而新建多级评论功能 里面需要关注下mptt的前端遍历树形的模板标签的写法

--- comment/models.py
+++ comment/models.py
@@ -2,15 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from article.models import Article
 from ckeditor.fields import RichTextField
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-# Create your models here.
-class Comment(models.Model):
+# 未使用树形结构的Comment模型
+# class Comment(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+#
+#     # 未使用富文本的定义
+#     # content = models.TextField()
+#
+#     # 使用富文本
+#     content = RichTextField()
+#
+#     created_time = models.DateTimeField(auto_now_add=True)
+#     updated_time = models.DateTimeField(auto_now=True)
+#
+#     class Meta:
+#         ordering = ['-created_time']
+#
+#     def __str__(self):
+#         return '<Comment: %s>' % self.content[:20]
+
+
+# 替换 models.Model 为 MPTTModel
+class Comment(MPTTModel):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-
-    # 未使用富文本的定义
-    # content = models.TextField()
 
     # 使用富文本
     content = RichTextField()
@@ -18,8 +37,15 @@ class Comment(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ['-created_time']
+    # mptt树形结构
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    # 记录二级评论回复给谁
+    reply_to = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='replyers')
+
+    # 替换Meta为MPTTMeat
+    class MPTTMeta:
+        order_insertion_by = ['created_time']
 
     def __str__(self):
         return '<Comment: %s>' % self.content[:20]
--- comment/views.py
+++ comment/views.py
@@ -4,10 +4,12 @@ from django.http import HttpResponse
 
 from article.models import Article
 from .form import CommentForm
+from .models import Comment
 
 
 # Create your views here.
 
+# 一级评论
 @login_required(login_url='userprofile/login')
 def post_comment(request, article_id):
     article = get_object_or_404(Article, id=article_id)
@@ -25,3 +27,46 @@ def post_comment(request, article_id):
             return redirect(article)
         else:
             return HttpResponse("表单内容有误，请重新填写")
+
+# 多级评论
+@login_required(login_url='userprofile/login')
+def post_comment(request, article_id, parent_comment_id=None):
+    article = get_object_or_404(Article, id=article_id)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.article = article
+            new_comment.user = request.user
+
+            # 二级评论
+            if parent_comment_id:
+                parent_comment = Comment.objects.get(id=parent_comment_id)
+
+                # 若回复层级超过耳机，则转换为二级
+                new_comment.parent_id = parent_comment.get_root().id
+
+                # 被回复人
+                new_comment.reply_to = parent_comment.user
+                new_comment.save()
+                return HttpResponse('200 OK')
+
+            new_comment.save()
+
+            # 当其参数是一个Model对象时，会自动调用这个Model对象的get_absolute_url()方法
+            return redirect(article)
+        else:
+            return HttpResponse("表单内容有误，请重新填写")
+
+    elif request.method == 'GET':
+        comment_form = CommentForm()
+        context = {}
+        context['comment_form'] = comment_form
+        context['article_id'] = article_id
+        context['parent_comment_id'] = parent_comment_id
+        return render(request, 'comment/reply.html', context)
+
+    else:
+        return HttpResponse("仅接受GET/POST请求")
